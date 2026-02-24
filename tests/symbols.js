74a52842a0ed9f0a3fe8a7b522d0e30561879239
akasimov <akasimov@strongteam.tech>
2026-02-24 20:42:33 +0600

    fix comment

--- tests/symbols.js
+++ tests/symbols.js
@@ -2,7 +2,7 @@
  * SPDX-FileCopyrightText: Copyright (c) 2024-2026 Zerocracy
  * SPDX-License-Identifier: MIT
  *
- * This file uses XML-special characters (&, &&, <, >) in JavaScript operators.
+ * This file uses XML-special characters (&, <, >) in JavaScript operators.
  * It exists solely to verify that CDATA wrapping handles them correctly.
  */
 
